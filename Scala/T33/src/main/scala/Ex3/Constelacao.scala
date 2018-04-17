8bb667f00a7576d8c04783388cc9ba30be3e6aeb
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2018-04-17 01:38:01 -0300

    arrumando o ex 3 em scala

--- Scala/T33/src/main/scala/Ex3/Constelacao.scala
+++ Scala/T33/src/main/scala/Ex3/Constelacao.scala
@@ -2,45 +2,30 @@ package Ex3
 
 import java.util.ArrayList
 
-import scala.collection.JavaConverters._
+class Constelacao(private var nome: String, maxEstrela: Int) {
 
-class Constelacao(private var nome: String) {
+  private var estrelas: Array[Estrela] = new Array[Estrela](maxEstrela)
 
-  private var nomes: ArrayList[Estrela] = new ArrayList[Estrela]()
-
-  private var temps: ArrayList[Estrela] = new ArrayList[Estrela]()
-
-  private var cores: ArrayList[Estrela] = new ArrayList[Estrela]()
+  private var qtAtual: Int = _
 
   def tempConstelacao(): Double = {
     var soma: Double = 0.0
-    for (e <- temps.asScala) {
-      soma += e.getTemperatura
+    for (e <- 0 until qtAtual) {
+      soma += estrelas(e).getTemperatura
     }
     soma
   }
 
   def adicionar(e: Estrela): Unit = {
-    nomes.add(e)
-    temps.add(e)
-    cores.add(e)
+    estrelas({ qtAtual += 1; qtAtual - 1 }) = e
   }
 
   def infoEstrelas(): Unit = {
-    println("Nomes das estrelas:")
-    for (e <- nomes.asScala) {
-      println(e.getNome)
+    for (e <- estrelas) {
+      println(
+        "Infos das Estrelas: " + e.getNome + e.getTemperatura +
+          e.getCor)
     }
-    println("-------------------------------")
-    println("Temperaturas das estrelas:")
-    for (e <- temps.asScala) {
-      println(e.getTemperatura)
-    }
-    println("-------------------------------")
-    println("Cores das estrelas:")
-    for (e <- cores.asScala) {
-      println(e.getCor)
-    }
-    println("-------------------------------")
   }
+
 }
--- Scala/T33/src/main/scala/Ex3/Principal.scala
+++ Scala/T33/src/main/scala/Ex3/Principal.scala
@@ -3,7 +3,7 @@ package Ex3
 object Principal {
 
   def main(args: Array[String]): Unit = {
-    val c: Constelacao = new Constelacao("Nome Constelação")
+    val c: Constelacao = new Constelacao("Nome Constelação", 3)
     val e0: Estrela = new Estrela("Endor", 45.5, "Azul")
     val e1: Estrela = new Estrela("Tatooine 1", 32.1, "Vermelho")
     val e2: Estrela = new Estrela("Coruscant", 20, "Verde")
